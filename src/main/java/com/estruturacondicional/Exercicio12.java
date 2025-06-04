1d0c298cc0fad7f86abe90158392cd198aeb4d63
Alisson <alissomacf@gmail.com>
2025-06-03 21:26:19 -0300

    Exercícios novos e correções

--- src/main/java/com/estruturacondicional/Exercicio12.java
+++ src/main/java/com/estruturacondicional/Exercicio12.java
@@ -13,12 +13,18 @@ public class Exercicio12 {
         System.out.println("Digite o terceiro número");
         int numero3 = scanner.nextInt();
 
-        if ((numero1 > numero2) && (numero1 > numero3)) {
-            System.out.println("O número maior é: " + numero1);
-        } else if ((numero2 > numero1) && (numero2 > numero3)) {
-            System.out.println("O número maior é: " + numero2);
-        } else if ((numero3 > numero1) && (numero3 > numero2)) {
-            System.out.println("O número maior é: " + numero3);
+        if ((numero1 == numero2) || (numero1 == numero3) || (numero2 == numero3)) {
+            System.out.println("Não é permitido números iguais.");
+        }else {
+            if ((numero1 > numero2) && (numero1 > numero3)) {
+                System.out.println("O maior número é: " + numero1);
+            }else if (numero2 > numero3) {
+                System.out.println("O número maior é: " + numero2);
+            }else {
+                System.out.println("O número maior é: " + numero3);
+            }
         }
     }
 }
+
+
---
+++ src/main/java/com/estruturacondicional/Exercicio14.java
@@ -0,0 +1,27 @@
+package main.java.com.estruturacondicional;
+
+import java.util.Scanner;
+
+public class Exercicio14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite outro número:");
+        int numero2 = scanner.nextInt();
+
+        if ((numero1 == 0) || (numero2 == 0)) {
+            System.out.println("Z");
+        } else if ((numero1 >= 0) && (numero2 >= 0)) {
+            System.out.println("M");
+        } else if ((numero1 < 0) && (numero2 < 0)) {
+            System.out.println("M");
+        } else if ((numero1 >= 0) && (numero2 < 0)) {
+            System.out.println("O");
+        } else if ((numero2 >= 0) && (numero1 < 0)) {
+            System.out.println("O");
+
+        }
+    }
+}
